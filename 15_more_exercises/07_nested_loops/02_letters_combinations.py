771ed85f85aae3605711e839cc762a477122f884
IvaVangelova <132647572+IvaVangelova@users.noreply.github.com>
2023-09-28 17:23:19 +0300

    Update 02_letters_combinations.py

--- 15_more_exercises/07_nested_loops/02_letters_combinations.py
+++ 15_more_exercises/07_nested_loops/02_letters_combinations.py
@@ -2,4 +2,18 @@ start = input()
 end = input()
 without = input()
 combination_counter = 0
-
+combo_start = ''
+combo_middle = ''
+combo_end = ''
+for i in range(ord(start), ord(end) + 1):
+    combo_start = chr(i)
+    for j in range(ord(start), ord(end) + 1):
+        combo_middle = chr(j)
+        for k in range(ord(start), ord(end) + 1):
+            combo_end = chr(k)
+            total = combo_start + combo_middle + combo_end
+            if without in total:
+                continue
+            print(total, end=' ')
+            combination_counter += 1
+print(combination_counter)
